022706313a9531ed6027f812bc2e841c7f131410
shorupandna <34935249+salil0001@users.noreply.github.com>
2019-04-10 04:32:20 +0530

    10 april

---
+++ src/Contain/Buttons/Buttons.js
@@ -0,0 +1,75 @@
+
+import React, { Component } from 'react'
+import './Buttons.css';
+export default class Buttons extends Component {
+    state = {
+        value: 1,
+        previous: true,
+        next: false
+    }
+    componentDidMount(){
+        this.setState({
+            value:this.props.pages
+        })
+      //  console.log("Component",this.state.value);
+    }
+
+    Buttonval = (e) => {
+        console.log("vale of changeAPI", this.state.value);
+        if (e === 1   ) {
+            this.setState({
+                value:e,
+                previous: true,
+                next: false
+              },
+             this.props.changeApi
+              )
+
+        }
+        else if (e === 4) {
+            this.setState({
+                value:e,
+                next: true,
+                previous: false,
+                
+            },
+            this.props.changeApi
+            
+            )
+
+        }
+        else {
+            this.setState({
+                value:e,
+                previous: false,
+                next: false,
+               
+            },
+            this.props.changeApi
+            )
+
+        }
+
+
+
+
+    }
+
+    render() {
+        const { changeApi } = this.props;
+           
+
+
+        return (
+            <div className="row rowamend" role="group" aria-label="First group">
+                <button onClick={() => { changeApi("Previous");this.Buttonval(this.props.pages)}} type="button" disabled={this.state.previous} className="btn btn-secondary mr-2">Previous</button>
+                <button onClick={() => { changeApi(1); this.Buttonval(1) }} type="button" className="btn btn-secondary mr-2 ">1</button>
+                <button onClick={() => { changeApi(2); this.Buttonval(2) }} type="button" className="btn btn-secondary mr-2">2</button>
+                <button onClick={() => { changeApi(3); this.Buttonval(3) }} type="button" className="btn btn-secondary mr-2">3</button>
+                <button onClick={() => { changeApi(4); this.Buttonval(4) }} type="button" className="btn btn-secondary mr-2">4</button>
+                <button onClick={() => changeApi("Next")} type="button" disabled={this.state.next} className="btn btn-secondary mr-2">Next</button>
+
+            </div>
+        )
+    }
+}
---
+++ src/Contain/Contain.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react'
+import Buttons from './Buttons/Buttons';
+import axios from 'axios';
+import './Contai.css'
+export default class Contain extends Component {
+  state = {
+    names: [],
+    pages: 1,
+
+
+  }
+
+  changeApi = (page) => {
+
+    if (page === "Previous") {
+      this.setState({
+        pages: this.state.pages - 1
+      },
+        this.callApiAgain
+      )
+    }
+    else if (page === "Next") {
+      this.setState({
+        pages: this.state.pages + 1
+      },
+        this.callApiAgain
+      )
+    }
+    else  {
+
+      this.setState({
+        pages: page
+      },
+        this.callApiAgain
+      )
+    }
+
+    console.log("hhh ",this.state.pages)
+  }
+
+  //console.log(this.state.pages)
+
+
+  callApiAgain = () => {
+    axios.get(`https://reqres.in/api/users?page=${this.state.pages}`)
+      .then(res => {
+        this.setState({
+          names: res.data.data
+
+        })
+
+        // console.log("API ",res.data.data);
+      }
+
+
+      )
+  }
+
+  componentDidMount() {
+    this.callApiAgain();
+  }
+  render() {
+    const { names } = this.state;
+    const nameList = names.length ? (
+      names.map(name => {
+        return (
+
+          <div className="col-4" key={name.id}>
+            <div className="card" style={{ width: "15rem" }}>
+              <img src={name.avatar} className="editImage" alt="Card image cap" />
+              <div className="card-body">
+                <h5 className="card-title">{name.first_name} {name.last_name}</h5>
+              </div>
+            </div>
+          </div>
+
+        )
+      })
+    ) :
+      (
+        <div>No Names Yet</div>
+
+      )
+    return (
+
+
+
+      <div>
+        <div className="container">
+          <div className="row justify-content-center">
+            {nameList}
+          </div>
+        </div>
+
+        <div style={{ paddingTop: "15px" }}>
+
+          <Buttons changeApi={this.changeApi} pages={this.state.pages}/>
+        </div>
+      </div>
+    )
+  }
+}
